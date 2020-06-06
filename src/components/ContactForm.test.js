feecf18281ae0e0135793ddb307e8178ca797767
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-06-05 17:03:45 -0700

    test fails getting form labels

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render, screen, fireEvent} from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+test('contact form renders', ()=>{
+    render (<ContactForm />);
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i); 
+    const emailInput = screen.getByLabelText(/email/i); 
+    const messageInput = screen.getByLabelText(/message/i); 
+
+
+})
